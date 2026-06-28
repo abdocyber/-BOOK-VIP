f4f0bd57c61c19bbdffbca29781b36020b17c1ac
abdocyber <abdo.0112432188@gmail.com>
2026-06-28 12:20:43 +0200

    Update import path for nav_helper.dart

--- BOOK2/lib/pages/notify_page.dart
+++ BOOK2/lib/pages/notify_page.dart
@@ -1,4 +1,4 @@
-import '../nav_helper.dart';
+import 'package:bankak_flutter_final/nav_helper.dart';
 import 'package:flutter/material.dart';
 import '../main.dart';
 import '../services/firebase_service.dart';
